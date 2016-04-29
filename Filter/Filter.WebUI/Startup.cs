0482be8b4687e06a617ae32cae2095bf46f675fb
ppypenko <ppypenko@student.neumont.edu>
2016-04-29 12:02:38 -0600

    Added project

---
+++ Filter/Filter.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Filter.WebUI.Startup))]
+namespace Filter.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
