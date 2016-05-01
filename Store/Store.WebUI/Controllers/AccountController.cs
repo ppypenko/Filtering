48984849fbebbd7c3704d7ea2293dc322e6d7ad7
ppypenko <ppypenko@student.neumont.edu>
2016-05-01 08:38:19 -0600

    Images show on all pages.

--- Store/Store.WebUI/Controllers/AccountController.cs
+++ Store/Store.WebUI/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace Store.WebUI.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Products");
                 }
                 AddErrors(result);
             }
@@ -392,7 +392,7 @@ namespace Store.WebUI.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Products");
         }
 
         //
@@ -449,7 +449,7 @@ namespace Store.WebUI.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Products");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
