e3195ad68977e9b865015107e63c64729e04243e
ppypenko <ppypenko@student.neumont.edu>
2016-05-01 06:58:23 -0600

    Redo.

--- Filter/Filter.WebUI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Filter.WebUI.MvcApplication" Language="C#" %>
---
+++ Store/Store.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Store.WebUI.Startup))]
+namespace Store.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
