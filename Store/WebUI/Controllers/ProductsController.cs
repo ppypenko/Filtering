fceb86d313a54db5a1a30aed7ea6d4413ac5770d
ppypenko <ppypenko@student.neumont.edu>
2016-05-04 13:34:13 -0600

    Added images, product controller/views and filtering

---
+++ Store/WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Data.Entities;
+using Microsoft.AspNet.Identity;
+
+namespace WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private StoreMasterEntities db = new StoreMasterEntities();
+
+        // GET: Products
+        public ActionResult Index(string productCategory, string searchString)
+        {
+            var CategoryQry = from m in db.Products orderby m.Category select m.Category;
+
+            var CategoryList = new List<string>();
+            CategoryList.AddRange(CategoryQry.Distinct());
+            ViewData["productCategory"] = new SelectList(CategoryList);
+
+            var products = from m in db.Products select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(p => p.Name.Contains(searchString));
+            }
+            if (!string.IsNullOrEmpty(productCategory))
+            {
+                products = products.Where(x => x.Category == productCategory);
+            }
+
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            //---------------start tracking views---------------
+            string user = User.Identity.GetUserId();
+            if (string.IsNullOrEmpty(user))
+            {
+                Boolean check = db.ViewedProducts.Where(c => c.UserID == user).Where(c => c.ProductID == (int)id).Any();
+                if(!check)
+                {
+                    ViewedProduct v = new ViewedProduct();
+                    v.ProductID = (int)id;
+                    v.UserID = user;
+                    db.ViewedProducts.Add(v);
+                    db.SaveChanges();
+                }
+            }
+            ViewBag.Product = product;
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,Name,Image,Description,Category,Price,Rating")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,Name,Image,Description,Category,Price,Rating")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
